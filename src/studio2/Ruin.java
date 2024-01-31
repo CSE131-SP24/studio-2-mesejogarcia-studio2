8b91cf7f4cfc3d760d24fd7c42ddce1458386de8
kikemesejo <kikemesejo@localhost>
2024-01-31 14:53:27 -0600

    incomplete

---
+++ src/studio2/Ruin.java
@@ -0,0 +1,49 @@
+package studio2;
+
+import java.util.Scanner;
+
+public class Ruin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Scanner in = new Scanner(System.in);
+	System.out.print("Investment: ");
+	double originalStart = in.nextDouble();
+	System.out.print("What's the win probability $$$$$$ ");
+	double winChance = in.nextDouble();
+	System.out.print("What's the win limit? ");
+	int winLimit = in.nextInt();
+	int day = 0;
+	int attempts = 0;
+	int number;
+	double startAmount = originalStart;
+	for (int totalSimulations = 1; totalSimulations<=100; totalSimulations++);
+	{
+		day++;
+	while ((startAmount<winLimit)&&(startAmount>0))
+	{
+		attempts++;
+		number = (int)(Math.random()*100-1);
+		if (number <= winChance*100)
+		{
+			startAmount++;
+
+		}
+		else 
+		{
+			startAmount--;
+		}
+	}
+	if (startAmount==winLimit)
+	{
+		System.out.println("Simulation "+day+": "+attempts+" WIN");
+	}
+	else
+	{
+		System.out.println("Simulation "+day+": "+attempts+" Big L Bozo");
+	}
+	startAmount = originalStart;
+	}
+}
+	}
+
